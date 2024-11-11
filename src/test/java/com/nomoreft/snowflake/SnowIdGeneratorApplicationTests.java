c23690513583d23fd3835ecf15cb8aeb70f86ef7
nomoreFt <hyunwoo.kim1108@gmail.com>
2024-11-11 20:57:30 +0900

    Init

---
+++ src/test/java/com/nomoreft/snowflake/SnowIdGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomoreft.snowflake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowIdGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
